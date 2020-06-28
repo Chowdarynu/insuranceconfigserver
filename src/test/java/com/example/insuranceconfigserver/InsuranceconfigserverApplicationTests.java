7d97a581b731b7594ad2934e7c7442997f04cc16
Narasimha Chowdary <Narasimha.Chowdary@ig.com>
2020-06-28 17:26:23 +0530

    first

---
+++ src/test/java/com/example/insuranceconfigserver/InsuranceconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insuranceconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceconfigserverApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
